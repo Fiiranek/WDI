487e8b949e06c8b201196244bf86952ea4c33896
Franciszek Job <jellybean@onet.eu>
2022-10-17 21:08:49 +0200

    fix: fixed zestaw_1/z_2.py

--- zestaw_1/z_2.py
+++ zestaw_1/z_2.py
@@ -1,42 +1,13 @@
-base_a = 1
-base_b = 1
-a = 1
-b = 1
-toggle = True
-is_2022 = False
-best = (1,2021)
-while not is_2022:
-    print(f"a={base_a} b={base_b}")
-    print("-------------")
-    while b <= 2022:
-        print(a)
-        c = a + b
-        a = b
-        b = c
-        if a == 2022:
-            print(f"found 2022, a={base_a} b={base_b}")
-            is_2022=True
-            break
-    # if toggle:
-    #     base_b += 1
-    # else:
-    #     base_a += 1
-    base_b+=1
-    a,b = base_a,base_b
-    toggle = not toggle
-#
-
-# suma = 10000
-# rok = 2022
-# best = (rok, 0)
-#
-# for i in range(rok+1):
-# 	y = rok
-# 	x = i
-# 	while x > 0 and y > x:
-# 		x, y = y-x, x
-# 	if x + y < suma:
-# 		suma = x + y
-# 		best = (x, y)
-#
-# print(suma, best)
+year = 2022
+best = [1, year - 1]
+for x in range(1, year + 1):
+    for y in range(1, year + 1):
+        a = y
+        b = x
+        while b < year + 1:
+            if b == year:
+                if sum([x, y]) < sum(best): best = [x, y]
+            c = a + b
+            b = a
+            a = c
+print(best)
