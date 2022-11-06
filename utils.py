da58971a3da9b996b1a569be65533252e2409f8d
Franciszek Job <jellybean@onet.eu>
2022-11-06 14:01:14 +0100

    refactor: fixed is_prime function

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ def digits_number(n: int):
 
 def is_prime(n):
     if n < 2: return False
+    if n == 2 or n == 3: return True
     if n % 2 == 0 or n % 3 == 0: return False
     x = 5
     while x < math.sqrt(n):
